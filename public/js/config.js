fbdc482620a10fffbddb1151335ee60a5ea51eac
W3BDevstack <118926098+devstackweb3@users.noreply.github.com>
2023-12-12 19:06:23 +0100

    Update config.js

--- public/js/config.js
+++ public/js/config.js
@@ -1,2 +1,4 @@
 export const PROJECT_ID = '0867l6ks'
 export const DATASET = 'production'
+//REASON OF COMMIT:  
+//https://www.sanity.io/answers/hello-quick-question-is-it-safe-to-commit-p1609342625280000
